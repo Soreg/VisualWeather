4055c6192a2b8f294337df6b543faeb5a798b36e
Soren Gravesen <S_Gravesen92@hotmail.com>
2017-08-14 01:47:06 +0200

    Animations: added 2 more layers of clouds to Cloudy card

--- js/animate.js
+++ js/animate.js
@@ -26,16 +26,34 @@ function fadeCardIn() {
 */
 function animateCloudy(i) {
   // target .data to make clouds
-  $("#forecastDay" + [i]).prepend("<div class='cloudy cloud1'></div>");
+  $("#forecastDay" + [i]).prepend("<div class='clouds'><div class='cloud1'></div><div class='cloud2'></div><div class='cloud3'></div></div>");
 
   // Animate cloud 1
   var cloudy1anim = function() {
-    $(".cloudy .cloud1").animate({"left": "-80px"}, 2000, function() {
-        $(this).animate({"left": "-30px"}, 1500)
+    $(".clouds .cloud1").animate({"left": "-80px"}, 2500, function() {
+        $(this).animate({"left": "-30px"}, 2200)
     })
- setTimeout(cloudy1anim, 3500); // added back
+ setTimeout(cloudy1anim, 4700); // added back
 }
+// Animate cloud 2
+var cloudy2anim = function() {
+  $(".clouds .cloud2").animate({"left": "-70px"}, 2500, function() {
+      $(this).animate({"left": "-50px"}, 2700)
+  })
+setTimeout(cloudy2anim, 5200); // added back
+}
+// Animate cloud 3
+var cloudy3anim = function() {
+  $(".clouds .cloud3").animate({"left": "-50px"}, 3600, function() {
+      $(this).animate({"left": "-70px"}, 4000)
+  })
+setTimeout(cloudy3anim, 7600); // added back
+}
+
+// Run animations
 cloudy1anim();
+cloudy2anim();
+cloudy3anim();
 }
 
 /*
