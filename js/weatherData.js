9b4fd58c6e4f62fcf335b1bacab85a697745527f
Soren Gravesen <S_Gravesen92@hotmail.com>
2017-08-13 15:56:54 +0200

    Data handling: Added all possible daily scenarios to the for-loop (default: fog)

--- js/weatherData.js
+++ js/weatherData.js
@@ -33,5 +33,30 @@ var days = [
 function editCards(days) {
   // For loop to assign data to every card
   for (var i = 0; i < days.length; i++) {
+    // Check to see weather states
+    /* -- CLOUDY -- */
+    if (days[i].icon === "partly-cloudy-day" || days[i].icon === "partly-cloudy-night" || days[i].icon === "cloudy") {
+
+    }
+    /* -- SUNNY -- */
+    else if (days[i].icon === "clear-day" || days[i].icon === "clear-night") {
+
+    }
+    /* -- RAIN -- */
+    else if (days[i].icon === "rain") {
+      
+    }
+    /* -- SNOW -- */
+    else if (days[i].icon === "snow" || days[i].icon === "sleet") {
+
+    }
+    /* -- WINDY -- */
+    else if (days[i].icon === "wind") {
+
+    }
+    /* -- FOG -- */
+    else {
+
+    }
   }
 }
