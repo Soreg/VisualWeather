d050df6b32d546a820e5af51c0336d19b0a982e2
Soren Gravesen <S_Gravesen92@hotmail.com>
2017-08-14 16:24:58 +0200

    Style: Changed styling for animated sun

--- js/animate.js
+++ js/animate.js
@@ -30,24 +30,24 @@ function animateCloudy(i) {
 
   // Animate cloud 1
   var cloudy1anim = function() {
-    $(".clouds .cloud1").animate({"left": "-80px"}, 2500, function() {
-        $(this).animate({"left": "-30px"}, 2200)
+    $(".clouds .cloud1").animate({left: "-80px"}, 2500, function() {
+        $(this).animate({left: "-30px"}, 2200)
     })
- setTimeout(cloudy1anim, 4700); // added back
+ setTimeout(cloudy1anim, 4700);
 }
 // Animate cloud 2
 var cloudy2anim = function() {
   $(".clouds .cloud2").animate({"left": "-70px"}, 2500, function() {
-      $(this).animate({"left": "-50px"}, 2700)
+      $(this).animate({left: "-50px"}, 2700)
   })
-setTimeout(cloudy2anim, 5200); // added back
+setTimeout(cloudy2anim, 5200);
 }
 // Animate cloud 3
 var cloudy3anim = function() {
-  $(".clouds .cloud3").animate({"left": "-50px"}, 3600, function() {
-      $(this).animate({"left": "-70px"}, 4000)
+  $(".clouds .cloud3").animate({left: "-50px"}, 3600, function() {
+      $(this).animate({left: "-70px"}, 4000)
   })
-setTimeout(cloudy3anim, 7600); // added back
+setTimeout(cloudy3anim, 7600);
 }
 // Run animations
 // Cloudy
@@ -65,10 +65,10 @@ function animateSunny(i) {
   $("#forecastDay" + [i]).prepend("<div class='sun'></div>");
   // Animate sun
   var sunnyanim = function() {
-    $(".sun").animate({"width": "80px", "height": "80px"}, 1500, function() {
-        $(this).animate({"width": "110px", "height": "110px"}, 3000)
+    $(".sun").animate({width: "80px", height: "80px"}, 1500, function() {
+        $(this).animate({width: "110px", height: "110px"}, 3000)
     })
- setTimeout(sunnyanim, 4500); // added back
+ setTimeout(sunnyanim, 4500);
 }
 
 sunnyanim();
