518d4aad5291ec70bed5deb95bf30824645a3d76
Soren Gravesen <S_Gravesen92@hotmail.com>
2017-08-15 14:54:52 +0200

    Bug-fix: Fixed animations being delayed.

--- js/animate.js
+++ js/animate.js
@@ -30,25 +30,25 @@ function animateCloudy(i) {
 
   // Animate cloud 1
   var cloudy1anim = function() {
-    $(".cloudy .clouds .cloud1").animate({left: "-80px"}, 2500, function() {
-        $(this).animate({left: "-30px"}, 2200)
-    })
- setTimeout(cloudy1anim, 4700);
-}
+  $(".cloudy .clouds .cloud1")
+    .animate({left: "-80px"}, 2500)
+    .animate({left: "-30px"}, 2200, cloudy1anim);
+  }
+
 // Animate cloud 2
-var cloudy2anim = function() {
-  $(".cloudy .clouds .cloud2").animate({"left": "-70px"}, 2500, function() {
-      $(this).animate({left: "-50px"}, 2700)
-  })
-setTimeout(cloudy2anim, 5200);
-}
+  var cloudy2anim = function() {
+  $(".cloudy .clouds .cloud2")
+    .animate({left: "-70px"}, 2500)
+    .animate({left: "-50px"}, 2700, cloudy2anim);
+  }
+
 // Animate cloud 3
-var cloudy3anim = function() {
-  $(".cloudy .clouds .cloud3").animate({left: "-50px"}, 3600, function() {
-      $(this).animate({left: "-70px"}, 4000)
-  })
-setTimeout(cloudy3anim, 7600);
-}
+  var cloudy3anim = function() {
+  $(".cloudy .clouds .cloud3")
+    .animate({left: "-50px"}, 3600)
+    .animate({left: "-70px"}, 4000, cloudy3anim);
+  }
+
 // Cloudy animations
 cloudy1anim();
 cloudy2anim();
@@ -63,11 +63,10 @@ function animateSunny(i) {
   $("#forecastDay" + [i]).prepend("<div class='sun'></div>");
   // Animate sun
   var sunnyanim = function() {
-    $(".sun").animate({width: "80px", height: "80px"}, 1500, function() {
-        $(this).animate({width: "110px", height: "110px"}, 3000)
-    })
- setTimeout(sunnyanim, 4500);
-}
+    $(".sun")
+      .animate({width: "80px", height: "80px"}, 1500)
+      .animate({width: "110px", height: "110px"}, 3000, sunnyanim);
+  }
 // Sunny animations
 sunnyanim();
 
@@ -82,18 +81,16 @@ function animateRain(i) {
 
   // Animate cloud 1
   var rainy1anim = function() {
-    $(".rain .clouds .cloud1").animate({left: "-50px"}, 3500, function() {
-        $(this).animate({left: "-60px"}, 3000)
-    })
- setTimeout(rainy1anim, 6500);
-}
+    $(".rain .clouds .cloud1")
+      .animate({left: "-50px"}, 3500)
+      .animate({left: "-60px"}, 3000, rainy1anim);
+  }
 // Animate cloud 2
 var rainy2anim = function() {
-  $(".rain .clouds .cloud2").animate({"left": "-70px"}, 2500, function() {
-      $(this).animate({left: "-50px"}, 2700)
-  })
-setTimeout(rainy2anim, 5200);
-}
+  $(".rain .clouds .cloud2")
+    .animate({left: "-70px"}, 2500)
+    .animate({left: "-50px"}, 2700, rainy2anim);
+  }
 // Rainy animations
 rainy1anim();
 rainy2anim();
